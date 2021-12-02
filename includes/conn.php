1ddb3a4e3452447b0e6874f623867ee88c3fbc72
Victor O. Morakinyo <morakinyovictor1@gmail.com>
2021-12-02 16:48:13 +0100

    update

--- includes/conn.php
+++ includes/conn.php
@@ -1,7 +1,7 @@
 <?php
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url(getenv("us-cdbr-east-04.cleardb.com"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
