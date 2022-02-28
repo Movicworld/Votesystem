05765b974d6dc6b50f6502d473514f0cab39022d
Victor O. Morakinyo <morakinyovictor1@gmail.com>
2022-02-28 16:55:50 +0100

    heroku

--- admin/voters_upload.php
+++ admin/voters_upload.php
@@ -2,19 +2,19 @@
 include 'includes/session.php';
 
 
-//     $sql = "INSERT INTO voters (voters_id, password, firstname, lastname, photo) VALUES ('$voter', '$password', '$firstname', '$lastname', '$filename')";
-//     if($conn->query($sql)){
-//         $_SESSION['success'] = 'Voter added successfully';
-//     }
-//     else{
-//         $_SESSION['error'] = $conn->error;
-//     }
-// 
-// }
-// else{
-//     $_SESSION['error'] = 'Fill up add form first';
-// }
+    $sql = "INSERT INTO voters (voters_id, password, firstname, lastname, photo) VALUES ('$voter', '$password', '$firstname', '$lastname', '$filename')";
+    if($conn->query($sql)){
+        $_SESSION['success'] = 'Voter added successfully';
+    }
+    else{
+        $_SESSION['error'] = $conn->error;
+    }
 
-// header('location: voters.php');
+}
+else{
+    $_SESSION['error'] = 'Fill up add form first';
+}
+
+header('location: voters.php');
 // 
 ?>
